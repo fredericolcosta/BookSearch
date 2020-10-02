09515dd2578de3291bf88067d78e800d09757f33
Frederico Costa <>
2020-10-02 10:06:33 +0100

    Added models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=20)
+    
+class Category(models.Model):
+    name = models.CharField(20, unique=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    pub_date = models.DateField('date published')    
+    authors = models.ManyToManyField(Author)
+    category = models.ForeignKey(Category)
