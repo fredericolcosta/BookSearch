741ec28f7cddcb69a3f582840ea64a89208d71fb
Frederico Costa <>
2020-10-02 11:05:31 +0100

    Added simple view for books with pagination

--- library/models.py
+++ library/models.py
@@ -2,13 +2,22 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
     
 class Category(models.Model):
     name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13, unique=True)
     pub_date = models.DateField('date published')    
-    authors = models.ManyToManyField(Author)
+    authors = models.ManyToManyField(Author, blank=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
--- library/views.py
+++ library/views.py
@@ -1,3 +1,13 @@
+from django.http import request
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Book
 
 # Create your views here.
+def index(request):
+    book_list = Book.objects.all()
+    paginator = Paginator(book_list, 2)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'library/index.html', {'page_obj': page_obj})
