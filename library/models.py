1cd0c63263f47912647d032608fd83e1c34a9106
Frederico Costa <>
2020-10-02 10:13:17 +0100

    Change in models

--- library/models.py
+++ library/models.py
@@ -4,11 +4,11 @@ class Author(models.Model):
     name = models.CharField(max_length=20)
     
 class Category(models.Model):
-    name = models.CharField(20, unique=True)
+    name = models.CharField(max_length=20, unique=True)
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13, unique=True)
     pub_date = models.DateField('date published')    
     authors = models.ManyToManyField(Author)
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
