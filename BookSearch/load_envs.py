8cf9fe4a8e22026154c29a9954bc44d440c31fd2
Frederico Costa <>
2020-10-02 09:11:47 +0100

    Initial commit

---
+++ BookSearch/load_envs.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+USER = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+NAME = os.getenv("NAME")
+
+print(USER, PASSWORD, HOST, PORT, NAME)
