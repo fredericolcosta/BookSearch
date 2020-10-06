d79d84874627a9d372017fd2064e94871bb272b6
Frederico Costa <>
2020-10-06 14:02:22 +0100

    Added authentication and filter books by category and author

--- library/urls.py
+++ library/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('book_detail/<int:pk>/', views.DetailView.as_view(), name='book_detail'),
     path('add_book', views.AddBook.as_view(), name='add_book'),
     path('update_book/<int:pk>/', views.UpdateBook.as_view(), name='update_book'),
+    path('login', views.LoginUser.as_view(), name='login'),
+    path('logout', views.LogoutUser.as_view(), name='logout'),
+    path('register', views.register_user, name='register'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,21 +1,42 @@
 from django.forms import models
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from django.contrib.auth.decorators import login_required
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import Book, Author, Category
 
 def index(request):
-    book_list = Book.objects.all().order_by('-title')
-    paginator = Paginator(book_list, 2)
+    categories = Category.objects.all()
+    
+    title = request.GET.get('title') 
+    title = '' if title is None else title
+    print(title)
+
+    author = request.GET.get('author')
+    author = '' if author is None else author
+    print(author)
+
+    category = request.GET.get('cat')
+
+    if category != None and category != 'all':
+            book_list = Book.objects.all().filter(category_id=category,authors__name__icontains=author,title__icontains=title).order_by('-title').distinct()
+    else:
+            book_list = Book.objects.all().filter(authors__name__icontains=author,title__icontains=title).order_by('-title').distinct()
+
+
+
+    paginator = Paginator(book_list, 5)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'library/index.html', {'page_obj': page_obj})
+    return render(request, 'library/index.html', {'page_obj': page_obj, "categories":categories})
 
 
 
@@ -24,8 +45,10 @@ class DetailView(generic.DetailView):
     template_name = 'library/book_detail.html'
 
   
-class AddBook(CreateView): 
+class AddBook(LoginRequiredMixin, CreateView): 
   
+    login_url = 'library:login'
+
     # specify the model for create view 
     model = Book 
   
@@ -35,10 +58,32 @@ class AddBook(CreateView):
     def get_success_url(self):
         return reverse('library:book_detail', kwargs={'pk': self.object.pk})
 
-class UpdateBook(UpdateView):
+class UpdateBook(LoginRequiredMixin, UpdateView):
+
+    login_url = 'library:login'
+
     model = Book
     fields = "__all__"
     template_name_suffix = '_update_form'
 
     def get_success_url(self):
         return reverse('library:book_detail', kwargs={'pk': self.object.pk})
+
+
+class LoginUser(LoginView):
+    template_name="library/login.html"
+
+class LogoutUser(LogoutView):
+    next_page = "library:index"
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('library:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'library/register.html', {'form': form})
+
+
